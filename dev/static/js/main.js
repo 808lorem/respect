9af37dd4432e436ac4bec4113ca7488ceabddd5d
Ilya Skryabin <808lorem@gmail.com>
2017-06-26 14:16:15 +0300

    Добавил секцию "Информация"

--- dev/static/js/main.js
+++ dev/static/js/main.js
@@ -3,13 +3,27 @@
 $(document).ready(function () {
 	svg4everybody({});
 	
-	$('ul.menu__sub:last').addClass('left');
-	// Определить отступ для выпадающего меню последней кнопки
-	var menuLinkWidth = $('li.menu__item:last').width(),
-		menuSubWidth = parseFloat($('ul.menu__sub:last').css('width')),
-		menuSubLeft = menuLinkWidth - menuSubWidth;
-	
-	$('ul.menu__sub:last').css('left', menuSubLeft + 'px');
+	$(window).resize(function(){
+		
+		var containerWidth = $('.container').css('width');
+		
+		if (containerWidth == '960px') {
+			$('nav.menu__nav').css({"display":'block'});
+			// Определить отступ для выпадающего меню последней кнопки
+			var menuLinkWidth = $('li.menu__item:last').width(),
+				menuSubWidth = parseFloat($('ul.menu__sub:last').css('width')),
+				menuSubLeft = menuLinkWidth - menuSubWidth;
+
+			$('ul.menu__sub:last').css('left', menuSubLeft + 'px');
+		} else {
+			$('ul.menu__sub:last').css('left', 'auto');
+			
+			$('nav.menu__nav').css({"display":'none'});
+		}
+	});
+
+	// вызовем событие resize
+	$(window).resize();
 });
 
 var maxHeight = 400;
@@ -20,9 +34,7 @@ $(function(){
     	
          var $container = $(this),
              $list = $container.find("ul"),
-             $anchor = $container.find("a"),
-             height = $list.height() * 1.1,       // make sure there is enough room at the bottom
-             multiplier = height / maxHeight;     // needs to move faster if list is taller
+             $anchor = $container.find("a");     // needs to move faster if list is taller
         
         // need to save height here so it can revert on mouseout            
         $container.data("origHeight", $container.height());
@@ -32,26 +44,7 @@ $(function(){
         
         // make sure dropdown appears directly below parent list item    
         $list
-            .show()
-            .css({
-                paddingTop: $container.data("origHeight")
-            });
-        
-        // don't do any animation if list shorter than max
-        if (multiplier > 1) {
-            $container
-                .css({
-                    height: maxHeight,
-                    overflow: "hidden"
-                })
-                .mousemove(function(e) {
-                    var offset = $container.offset();
-                    var relativeY = ((e.pageY - offset.top) * multiplier) - ($container.data("origHeight") * multiplier);
-                    if (relativeY > $container.data("origHeight")) {
-                        $list.css("top", -relativeY + $container.data("origHeight"));
-                    };
-                });
-        }
+            .show();
         
     }, function() {
     
@@ -61,7 +54,6 @@ $(function(){
         $el
             .height($(this).data("origHeight"))
             .find("ul")
-            .css({ top: 0 })
             .hide()
             .end()
             .find("a")
@@ -73,39 +65,12 @@ $(function(){
 
 
 $('.js-slick__slider').slick({
-  dots: true,
-  infinite: false,
-  speed: 300,
-  slidesToShow: 1,
-  slidesToScroll: 1,
-//  responsive: [
-//    {
-//      breakpoint: 1024,
-//      settings: {
-//        slidesToShow: 3,
-//        slidesToScroll: 3,
-//        infinite: true,
-//        dots: true
-//      }
-//    },
-//    {
-//      breakpoint: 600,
-//      settings: {
-//        slidesToShow: 2,
-//        slidesToScroll: 2
-//      }
-//    },
-//    {
-//      breakpoint: 480,
-//      settings: {
-//        slidesToShow: 1,
-//        slidesToScroll: 1
-//      }
-//    }
-//    // You can unslick at a given breakpoint now by adding:
-//    // settings: "unslick"
-//    // instead of a settings object
-//  ]
+	dots: true,
+	infinite: false,
+	speed: 300,
+	arrows: false,
+	slidesToShow: 1,
+	slidesToScroll: 1,
 });
 
 
@@ -117,3 +82,8 @@ $('a.services__block').hover(
 		$(this).removeClass('hover');
 	}
 );
+
+$("a.menu__btn").click(function() {
+		$('nav.menu__nav').toggle("display");
+	return false;
+});
